156283d4f120d3726c214467400579a8ad74e7cd
Griffin <Griffin.Hotchkiss@gmail.com>
2020-08-26 14:13:09 +0630

    remove all truffle and npm stuff

--- migrations/2_deploy_token.js
+++
@@ -1,6 +0,0 @@
-const YourToken = artifacts.require("YourToken");
-
-module.exports = (deployer) => {
-  // default initialSupply is 1.0 yourTokens
-  deployer.deploy("YourToken", 1000000000000000000);
-};
--- migrations/3_provision_token.js
+++
@@ -1,7 +0,0 @@
-const YourToken = artifacts.require("YourToken");
-
-module.exports = async (deployer, network, accounts) => {
-  const token = await YourToken.deployed();
-
-  
-};
